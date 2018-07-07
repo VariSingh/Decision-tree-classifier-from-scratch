7c3a0280eeeec60724f7839bb2eb4e349132327d
Varinder Singh <varindersingh271990@gmail.com>
2018-07-07 20:26:08 +0530

    get unique values
    added a function to get unique values from dataset.

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-#write data
+//write data
+//numerical data (height,weight,shoe size)
+X = [[191, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], [171, 75, 42], [181, 85, 43]]
 
-X=[[191,80,44],[177,70,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[177,70,40],[171,75,42],[181,85,43]]
-Y=['male','female','female','female','male','male','male','female','female','male']
+//sample mix data (height,weight, favourite color)
+//X=[[191,80,'blue'],[177,70,'pink'],[160,60,'red'],[154,54,'red'],[166,65,'brown'],[190,90,'yellow'],[175,64,'black'],[177,70,'blue'],[171,75,'pink'],[181,85,'green']]
+
+//labels
+Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'female', 'male']
 
 
-#decision tree classifier
+//decision tree classifier
+
+function classifier(X, Y) {
+
+}
+
+
+funtion get_unique_values(data,column){
+  var _obj = {};
+  for (var i = 0; i < data.length; i++) {
+    _obj[training_data[i][column]] = training_data[i][column];
+  }
+  var _arr = []
+  for (item in _obj) {
+    arr.push(_obj[item]);
+  }
+  return arr;
+}
